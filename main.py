cab6e6188ef1c34bedba633983d7247f2ff6bad2
delacruz.dwyghtdani@gmail.com <156776807+Dwyght@users.noreply.github.com>
2025-03-20 14:46:35 +0800

    testing 1

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import os
 import cv2 as cv
 import numpy as np
 import pytesseract
-pytesseract.pytesseract.tesseract_cmd = "/usr/bin/tesseract"
 import json
 import re
 from spellchecker import SpellChecker
